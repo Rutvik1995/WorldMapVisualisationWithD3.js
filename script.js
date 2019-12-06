1a14c9697d2e3b8e74623d3f993971534f35c02f
gauravbora2008 <gauravbora2008@gmail.com>
2019-12-06 12:15:47 -0800

    partial commit

--- script.js
+++ script.js
@@ -182,11 +182,15 @@ var m = svg.append("g")
         .attr("transform","translate(15,200)")
         .append("text");
 
+var lineChartWorldBankData = {};
+
 function ready(error, topo, dataset, worldBankData) {
+  lineChartWorldBankData = worldBankData;
   renderMap(topo, worldBankData);
   cleanedWorldBankData = filterTopData(worldBankData);
   renderPieChart(cleanedWorldBankData);
   renderBarChart(cleanedWorldBankData);
+  renderLineChart(error, worldBankData);
 }
 
 function filterTopData(worldBankData) {
@@ -677,5 +681,251 @@ function renderBarChart(data) {
         .style("opacity", 0)
     );
 }
+
+var country1 = 'China';
+var country2 = 'United Kingdom';
+
+var lineChartWorldBankData = {};
+
+function countryInput1Changed(e) {
+  country1 = e.value;
+  console.log(e.value);
+  updateLineChart();
+}
+
+function countryInput2Changed(e) {
+  country2 = e.value;
+  console.log(e.value);
+  updateLineChart();
+}
+
+// d3.queue().defer(
+//   d3.csv,
+//   "https://raw.githubusercontent.com/ZeningQu/World-Bank-Data-by-Indicators/master/climate-change/climate-change.csv"
+// )
+// .await(readyline);
+
+function updateLineChart() {
+  error = '';
+  console.log(error, lineChartWorldBankData, country1, country2);
+  renderLineChart(error, lineChartWorldBankData, country1, country2);
+}
+
+function renderLineChart(error,worldBankData, country1, country2) {
+  lineChartWorldBankData = worldBankData;
+  console.log("readline function");
+  console.log(worldBankData);
+  loadFirstField(worldBankData, country1);
+
+    var width = 1350;
+    var height = 300;
+    var margin = 50;
+    var duration = 250;
+    var lineOpacity = "0.25";
+    var lineOpacityHover = "0.85";
+    var otherLinesOpacityHover = "0.1";
+    var lineStroke = "1.5px";
+    var lineStrokeHover = "2.5px";
+    var circleOpacity = '0.85';
+    var circleOpacityOnLineHover = "0.25"
+    var circleRadius = 3;
+    var circleRadiusHover = 6;
+
+  var parseDate = d3.timeParse("%Y");
+        dataColumn.forEach(function(d) { 
+          console.log(d);
+          d.values.forEach(function(d) {
+          d.date = parseDate(d.date);
+          d.urbanPopulation = +d.urbanPopulation;    
+        });
+      });
+    var xScale = d3.scaleTime()
+      .domain(d3.extent(dataColumn[0].values, d => d.date))
+      .range([0, width-margin]);
+
+    var yScale = d3.scaleLinear()
+      .domain([0, d3.max(dataColumn[0].values, d => d.urbanPopulation)])
+      .range([height-margin, 0]);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var svg = d3.select("#linechart").append("svg")
+    .attr("width", (width+margin)+"px")
+    .attr("height", (height+margin)+"px")
+    .append('g')
+    .attr("transform", `translate(${margin}, ${margin})`);
+
+
+
+    var line = d3.line()
+      .x(d => xScale(d.date))
+      .y(d => yScale(d.urbanPopulation));
+
+      let lines = svg.append('g')
+       .attr('class', 'lines');
+
+
+       lines.selectAll('.line-group')
+    .data(dataColumn).enter()
+      .append('g')
+    .attr('class', 'line-group')  
+    .on("mouseover", function(d, i) {
+  svg.append("text")
+    .attr("class", "title-text")
+    .style("fill", color(i))        
+    .text(d.CountryName)
+    .attr("text-anchor", "middle")
+    .attr("x", (width-margin)/2)
+    .attr("y", 5);
+})
+.on("mouseout", function(d) {
+  svg.select(".title-text").remove();
+})
+.append('path')
+.attr('class', 'line')  
+.attr('d', d => line(d.values))
+.style('stroke', (d, i) => color(i))
+.style('opacity', lineOpacity)
+.on("mouseover", function(d) {
+  d3.selectAll('.line')
+      .style('opacity', otherLinesOpacityHover);
+  d3.selectAll('.circle')
+      .style('opacity', circleOpacityOnLineHover);
+  d3.select(this)
+    .style('opacity', lineOpacityHover)
+    .style("stroke-width", lineStrokeHover)
+    .style("cursor", "pointer");
+})
+.on("mouseout", function(d) {
+  d3.selectAll(".line")
+      .style('opacity', lineOpacity);
+  d3.selectAll('.circle')
+      .style('opacity', circleOpacity);
+  d3.select(this)
+    .style("stroke-width", lineStroke)
+    .style("cursor", "none");
+});
+
+lines.selectAll("circle-group")
+.data(dataColumn).enter()
+.append("g")
+.style("fill", (d, i) => color(i))
+.selectAll("circle")
+.data(d => d.values).enter()
+.append("g")
+.attr("class", "circle")  
+.on("mouseover", function(d) {
+  d3.select(this)     
+    .style("cursor", "pointer")
+    .append("text")
+    .attr("class", "text")
+    .text(`${d.urbanPopulation}`)
+    .attr("x", d => xScale(d.date) + 5)
+    .attr("y", d => yScale(d.urbanPopulation) - 10);
+})
+.on("mouseout", function(d) {
+  d3.select(this)
+    .style("cursor", "none")  
+    .transition()
+    .duration(duration)
+    .selectAll(".text").remove();
+})
+.append("circle")
+.attr("cx", d => xScale(d.date))
+.attr("cy", d => yScale(d.urbanPopulation))
+.attr("r", circleRadius)
+.style('opacity', circleOpacity)
+.on("mouseover", function(d) {
+    d3.select(this)
+      .transition()
+      .duration(duration)
+      .attr("r", circleRadiusHover);
+  })
+.on("mouseout", function(d) {
+    d3.select(this) 
+      .transition()
+      .duration(duration)
+      .attr("r", circleRadius);  
+  });
+
+  var xAxis = d3.axisBottom(xScale).ticks(5);
+  var yAxis = d3.axisLeft(yScale).ticks(5);
+
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height-margin})`)
+    .call(xAxis);
+
+
+    svg.append("g")
+.attr("class", "y axis")
+.call(yAxis)
+.append('text')
+.attr("y", 15)
+.attr("transform", "rotate(-90)")
+.attr("fill", "#000")
+.text("Total values");
+
+}
+ 
+  var dataColumn=[];
+  var realData={
+    CountryName:"",
+    values:[]
+  };
+  var realData2={
+    CountryName:"",
+    values:[]
+  };
+  function loadFirstField(worldBankData, country1){
+    realData.CountryName = country1;
+    for (let i = 0; i < worldBankData.length; i++) {
+      var jj={
+        date:"",
+        urbanPopulation:""
+      }
+        if(worldBankData[i]['Year']>1990){
+            if(worldBankData[i]["Country Name"] == country1){
+            console.log("in if");
+            //realData.date.push(worldBankData[i]['Year']);
+            //realData.urbanPopulation.push(worldBankData[i]['Urban population (% of total)']);
+            jj.date=worldBankData[i]['Year'];
+            jj.urbanPopulation=worldBankData[i]['Urban population (% of total)']
+            realData.values.push(jj);
+          } 
+        }   
+    }
+    dataColumn.push(realData);
+    console.log(dataColumn);
+    loadSecondField(worldBankData, country2);
+  }
+
+  function loadSecondField(worldBankData, country2) {
+    realData2.CountryName = country2;
+    for (let i = 0; i < worldBankData.length; i++) {
+      var jj={
+        date:"",
+        urbanPopulation:""
+      }
+        if(worldBankData[i]['Year']>1990){
+          if(worldBankData[i]["Country Name"] == country2){
+            console.log("in if");
+            //realData2.date.push(worldBankData[i]['Year']);
+            //realData2.urbanPopulation.push(worldBankData[i]['Urban population (% of total)']);
+            jj.date=worldBankData[i]['Year'];
+            jj.urbanPopulation=worldBankData[i]['Urban population (% of total)']
+            realData2.values.push(jj);
+          } 
+        }
+    }      
+    dataColumn.push(realData2);
+    console.log(dataColumn);
+  }
+
+
+
+
+
 
 
